0a75ba3ea31d9272ef8a01bdfee3fda060e819f0
Diwakar <diwakarbarbigha@gmail.com>
2022-05-03 16:07:30 +0530

    DB TABLE & MAIN ACTIVITY CHANGES

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,49 +7,23 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button insert;
-    private Button view;
-    private Button feeddogs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("Pet Dogs");
-        insert = findViewById(R.id.insert);
-        view = findViewById(R.id.view);
-        feeddogs = findViewById(R.id.feeddogs);
 
-        insert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, View_Dogs.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Dogs_view.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        getSupportActionBar().hide(); // hide the title bar
 
-        feeddogs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Dogs_view.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+
+
 
     }
 
