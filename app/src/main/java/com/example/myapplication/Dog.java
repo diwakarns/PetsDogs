58338e3cf39fe9bc9726831841c1193a204db90f
Diwakar <diwakarbarbigha@gmail.com>
2022-05-03 22:51:33 +0530

    Dogs View

--- app/src/main/java/com/example/myapplication/Dog.java
+++ app/src/main/java/com/example/myapplication/Dog.java
@@ -3,13 +3,27 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Dog extends AppCompatActivity {
 
+    private Button Submitsociety;
+    private EditText createdby, editTextTextEmailAddress;
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dog);
+
+        Submitsociety = findViewById(R.id.Submitsociety);
+        createdby = findViewById(R.id.createdby);
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        title = findViewById(R.id.title);
+
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,17 +10,33 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView text;
+    private Button insert,view,feeddogs,viewdogs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         getSupportActionBar().hide(); // hide the title bar
+
+        text = findViewById(R.id.text);
+        insert = findViewById(R.id.insert);
+        view = findViewById(R.id.view);
+        feeddogs = findViewById(R.id.feeddogs);
+        viewdogs = findViewById(R.id.viewdogs);
+
+         insert.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent = new Intent(MainActivity.this, Dog.class);
+                 startActivity(intent);
+             }
+         });
 
 
 
